cea23c3433308d69efef657199e25289233d698c
Eliezer Cabezas <66449381+Eliezer-09@users.noreply.github.com>
2024-08-05 18:54:18 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lago-musical';
+  campecinoFrente = true;
+  sonidos: { [key in Animal]: string[] } = {
+    rana: ['brr', 'birip', 'brrah', 'croac'],
+    libelula: ['fiu', 'plop', 'pep'],
+    grillo: ['cric-cric', 'trri-trri', 'bri-bri']
+  };
+  currentSonidos: string[] = [];
+  currentSonidoIndex = 0;
+  mensaje = '';
+  showMensaje = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  mostrarSonidos(animal: Animal) {
+    this.currentSonidos = this.sonidos[animal];
+    this.currentSonidoIndex = 0;
+    this.campecinoFrente = true;
+  }
+
+  ocultarSonidos() {
+    this.currentSonidos = [];
+  }
+
+  reproducirSonido(sonido: string) {
+    this.campecinoFrente = false;
+    this.mensaje = sonido;
+    this.showMensaje = true;
+    this.currentSonidoIndex = 0;
+    this.animarSonidos();
+  }
+
+  animarSonidos() {
+    if (this.currentSonidoIndex < this.currentSonidos.length) {
+      this.mensaje = this.currentSonidos[this.currentSonidoIndex];
+      this.currentSonidoIndex++;
+      setTimeout(() => this.animarSonidos(), 1000);
+    } else {
+      this.campecinoFrente = true;
+      this.showMensaje = false;
+    }
+  }
 }
+
+type Animal = 'rana' | 'libelula' | 'grillo';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule // Asegúrate de importar CommonModule aquí
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
