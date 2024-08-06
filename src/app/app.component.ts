ceecd4236066b6b4ede7c01da2acf2aaef57c298
Eliezer Cabezas <66449381+Eliezer-09@users.noreply.github.com>
2024-08-05 19:05:36 -0500

    Mejoras de rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
