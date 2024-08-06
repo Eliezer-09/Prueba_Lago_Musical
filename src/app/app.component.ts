992d578e19a82bdd34cac0fafd46b9d897c87c72
Eliezer Cabezas <66449381+Eliezer-09@users.noreply.github.com>
2024-08-06 00:15:17 -0500

    Completar animación y giros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+type Animal = 'rana' | 'libelula' | 'grillo';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,45 +18,60 @@ export class AppComponent {
     libelula: ['fiu', 'plop', 'pep'],
     grillo: ['cric-cric', 'trri-trri', 'bri-bri']
   };
+  canciones: { [key: string]: string[] } = {
+    'brr': ['fiu', 'cric-cric', 'brrah'],
+    'birip': ['trri-trri', 'croac'],
+    'plop': ['cric-cric', 'brrah'],
+    'croac': ['zzz'],
+    'brrah': ['zzz']
+  };
   currentSonidos: string[] = [];
   currentSonidoIndex = 0;
   mensaje = '';
   showMensaje = false;
+  showOpciones = false;
   currentAnimal: Animal | null = null;
 
   constructor(private renderer: Renderer2) {}
 
-  mostrarSonidos(animal: Animal) {
-    this.currentSonidos = this.sonidos[animal];
-    this.currentSonidoIndex = 0;
-    this.campecinoFrente = true;
-    this.currentAnimal = animal;
-  }
-
-  ocultarSonidos() {
-    this.currentSonidos = [];
-    this.currentAnimal = null;
+  mostrarOpciones() {
+    this.showOpciones = true;
   }
 
   reproducirSonido(sonido: string) {
+    
+    this.showOpciones = false;
     this.campecinoFrente = false;
+    this.currentSonidos = this.canciones[sonido] || [];
     this.mensaje = sonido;
     this.showMensaje = true;
     this.currentSonidoIndex = 0;
+    this.currentAnimal = null;
     this.animarSonidos();
   }
 
   animarSonidos() {
     if (this.currentSonidoIndex < this.currentSonidos.length) {
-      this.mensaje = this.currentSonidos[this.currentSonidoIndex];
+      const siguienteSonido = this.currentSonidos[this.currentSonidoIndex];
+      this.mensaje = siguienteSonido;
       this.currentSonidoIndex++;
+
+      if (this.sonidos.rana.includes(siguienteSonido)) {
+        this.currentAnimal = 'rana';
+      } else if (this.sonidos.libelula.includes(siguienteSonido)) {
+        this.currentAnimal = 'libelula';
+      } else if (this.sonidos.grillo.includes(siguienteSonido)) {
+        this.currentAnimal = 'grillo';
+      }
+
       setTimeout(() => this.animarSonidos(), 1000);
     } else {
       this.campecinoFrente = true;
-      this.showMensaje = false;
-      this.ocultarSonidos();
+      this.mensaje = 'zzz'; // Mostrar zzz cuando termina la canción
+      this.currentAnimal = null; // Ocultar mensaje en los animales
+      setTimeout(() => {
+        this.showMensaje = false; // Ocultar el mensaje después de un tiempo
+      }, 1000);
     }
   }
 }
-
-type Animal = 'rana' | 'libelula' | 'grillo';
